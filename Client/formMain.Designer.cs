7abccbb8009b28aa9619a697dc2658a9a202a881
Shahzaib Hassan <shahzaib.hassan@ghostsoftware.local>
2021-05-23 17:22:16 +0500

    Add project files.

---
+++ Client/formMain.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Client
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.history = new System.Windows.Forms.TextBox();
+            this.input = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.privateChatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.indicator = new System.Windows.Forms.Panel();
+            this.btnClr = new System.Windows.Forms.Button();
+            this.pleaseTryAgainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lbl = new System.Windows.Forms.Label();
+            this.btntry = new System.Windows.Forms.Button();
+            this.btnnew = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // history
+            // 
+            this.history.Location = new System.Drawing.Point(12, 60);
+            this.history.Multiline = true;
+            this.history.Name = "history";
+            this.history.ReadOnly = true;
+            this.history.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.history.Size = new System.Drawing.Size(219, 150);
+            this.history.TabIndex = 0;
+            this.history.TextChanged += new System.EventHandler(this.history_TextChanged);
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(27, 216);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(161, 20);
+            this.input.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(197, 216);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(298, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(72, 134);
+            this.listBox1.TabIndex = 3;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.privateChatToolStripMenuItem,
+            this.pleaseTryAgainToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(157, 48);
+            // 
+            // privateChatToolStripMenuItem
+            // 
+            this.privateChatToolStripMenuItem.Name = "privateChatToolStripMenuItem";
+            this.privateChatToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.privateChatToolStripMenuItem.Text = "Private Chat";
+            this.privateChatToolStripMenuItem.Click += new System.EventHandler(this.privateChatToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(310, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Friends";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(298, 226);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(72, 23);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // indicator
+            // 
+            this.indicator.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.indicator.Location = new System.Drawing.Point(12, 12);
+            this.indicator.Name = "indicator";
+            this.indicator.Size = new System.Drawing.Size(10, 11);
+            this.indicator.TabIndex = 6;
+            this.indicator.BackColorChanged += new System.EventHandler(this.indicator_BackColorChanged);
+            // 
+            // btnClr
+            // 
+            this.btnClr.Location = new System.Drawing.Point(237, 189);
+            this.btnClr.Name = "btnClr";
+            this.btnClr.Size = new System.Drawing.Size(20, 21);
+            this.btnClr.TabIndex = 7;
+            this.btnClr.Text = "-";
+            this.btnClr.UseVisualStyleBackColor = true;
+            this.btnClr.Click += new System.EventHandler(this.btnClr_Click);
+            // 
+            // pleaseTryAgainToolStripMenuItem
+            // 
+            this.pleaseTryAgainToolStripMenuItem.Name = "pleaseTryAgainToolStripMenuItem";
+            this.pleaseTryAgainToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.pleaseTryAgainToolStripMenuItem.Text = "Please try again";
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.Location = new System.Drawing.Point(102, 25);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(0, 20);
+            this.lbl.TabIndex = 8;
+            // 
+            // btntry
+            // 
+            this.btntry.Location = new System.Drawing.Point(281, 188);
+            this.btntry.Name = "btntry";
+            this.btntry.Size = new System.Drawing.Size(102, 23);
+            this.btntry.TabIndex = 9;
+            this.btntry.Text = "Please Try Again";
+            this.btntry.UseVisualStyleBackColor = true;
+            this.btntry.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnnew
+            // 
+            this.btnnew.Location = new System.Drawing.Point(276, 265);
+            this.btnnew.Name = "btnnew";
+            this.btnnew.Size = new System.Drawing.Size(75, 23);
+            this.btnnew.TabIndex = 10;
+            this.btnnew.Text = "TRY";
+            this.btnnew.UseVisualStyleBackColor = true;
+            this.btnnew.Click += new System.EventHandler(this.btnnew_Click);
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(439, 321);
+            this.Controls.Add(this.btnnew);
+            this.Controls.Add(this.btntry);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.btnClr);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.history);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.indicator);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "formMain";
+            this.Text = "Client ";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.formMain_FormClosing);
+            this.Load += new System.EventHandler(this.formMain_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox history;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem privateChatToolStripMenuItem;
+        private System.Windows.Forms.Panel indicator;
+        private System.Windows.Forms.Button btnClr;
+        private System.Windows.Forms.ToolStripMenuItem pleaseTryAgainToolStripMenuItem;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Button btntry;
+        private System.Windows.Forms.Button btnnew;
+    }
+}
+
---
+++ Client/formMain.cs
@@ -0,0 +1,509 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Client
+{
+    public partial class formMain : Form
+    {
+
+       
+
+        public TcpClient clientSocket;
+        public NetworkStream serverStream = default(NetworkStream);
+
+        string readData = null;
+        Thread ctThread;
+        String name = null;
+        //Dictionary<string, Object> nowChatting = new Dictionary<string, Object>();
+        List<string> nowChatting = new List<string>();
+        List<string> chat = new List<string>();
+
+        public void setName(String title)
+        {
+            this.Text = title;
+            name = title;
+        }
+
+        public formMain()
+        {
+           
+            InitializeComponent();
+          
+
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // Connect to the specified host.
+
+
+
+            IPHostEntry host;
+            IPAddress LocalIP = null; string str = string.Empty;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress i in host.AddressList)
+            {
+                if (i.AddressFamily.ToString() == "InterNetwork")
+
+                {
+                    LocalIP = i;
+
+                    str = LocalIP.ToString();
+                    //    Console.WriteLine(str);
+                }
+
+                String Host = Dns.GetHostName();
+                IsADSAlive(str);
+                Ping myPing = new Ping();
+              //PingReply reply = myPing.Send(str, 5000);
+              //  if (reply != null)
+              //  {
+              //      string re = reply.Status.ToString();
+              //      MessageBox.Show($"Server is available {re}");
+
+              //      // Console.WriteLine("Status :  " + reply.Status + " \n Time : " + reply.RoundtripTime.ToString() + " \n Address : " + reply.Address);
+              //      //Console.WriteLine(reply.ToString());
+
+              //  }
+              //  else
+              //  {
+              //      MessageBox.Show("server is not available");
+              //  }
+
+            }
+
+        }
+        public static bool IsADSAlive(String hostName)
+        {
+            try
+            {
+                using (TcpClient tcpClient = new TcpClient())
+                {
+                    
+                    tcpClient.Connect(hostName, 5000);
+
+                    MessageBox.Show("Server is Available");
+
+                    return true;
+                    
+                }
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("Server is not available");
+                return false;
+            }
+        }
+
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!input.Text.Equals(""))
+                {
+                    chat.Add("gChat");
+                    chat.Add(input.Text);
+                    byte[] outStream = ObjectToByteArray(chat);
+
+                    serverStream.Write(outStream, 0, outStream.Length);
+                    serverStream.Flush();
+                    input.Text = "";
+                    chat.Clear();
+                }
+            }
+            catch (Exception er)
+            {
+                btnConnect.Enabled = true;
+            }
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            clientSocket = new TcpClient();
+            try
+            {
+                clientSocket.Connect("127.0.0.1", 5000);
+                readData = "Connected to Server ";
+
+
+                byte[] nm = new byte[283];
+                NetworkStream stre = clientSocket.GetStream(); //Gets The Stream of The Connection
+                                                               //  stre.Write(nm, 0, nm.Length);// send the msg
+
+
+                stre.Read(nm, 0, nm.Length);
+                String ack = Encoding.ASCII.GetString(nm);
+                stre.Flush();
+                //MessageBox.Show(ack);
+                // received data
+                //Socket list;
+                ////   sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                //list = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                //list.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000));
+                //list.Listen(0);
+                //Socket sk = list.Accept();
+                //byte[] buffer = new byte[1238];
+                //int y = sk.Receive(buffer, 0, buffer.Length, 0);
+                //Array.Resize(ref buffer, y);
+
+
+
+                //string ser= Encoding.ASCII.GetString(buffer);
+
+
+                Update(ack);
+                msg();
+
+                serverStream = clientSocket.GetStream();
+
+                byte[] outStream = Encoding.ASCII.GetBytes(name + "$");
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Flush();
+                btnConnect.Enabled = false;
+
+
+                ctThread = new Thread(getMessage);
+                ctThread.Start();
+            }
+            catch (Exception er)
+            {
+                Dateup(true);
+            //    MessageBox.Show("Server Not Started");
+            }
+        }
+
+        private void Dateup(bool v)
+        {
+            Invoke((Action)delegate
+                {
+                    lbl.Text = "Server is Disconnected";
+                    btnSend.Enabled = false;
+                    btnConnect.Enabled = false;
+                }
+
+                );
+        }
+
+        private void Update(string v)
+        {
+
+            Invoke((Action)delegate
+            {
+                lbl.Text = v;
+
+                    // label1.Visible = textBoxEmployee.Visible = !toggle;
+
+                    //  labelIP.Visible = textBoxAddress.Visible = !toggle;
+                });
+
+        }
+
+        public void getUsers(List<string> parts)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                listBox1.Items.Clear();
+                for (int i = 1; i < parts.Count; i++)
+                {
+                    listBox1.Items.Add(parts[i]);
+
+                }
+            });
+        }
+
+        private void getMessage()
+        {
+            try
+            {
+                while (true)
+                {
+                    serverStream = clientSocket.GetStream();
+                    byte[] inStream = new byte[10025];
+                    serverStream.Read(inStream, 0, inStream.Length);
+                    List<string> parts = null;
+
+                    if (!SocketConnected(clientSocket))
+                    {
+                        MessageBox.Show("You've been Disconnected");
+                        ctThread.Abort();
+                        clientSocket.Close();
+                        btnConnect.Enabled = true;
+                    }
+
+                    parts = (List<string>)ByteArrayToObject(inStream);
+                    switch (parts[0])
+                    {
+                        case "userList":
+                            getUsers(parts);
+                            break;
+
+                        case "gChat":
+                            readData = "" + parts[1];
+                            msg();
+                            break;
+
+                        case "pChat":
+                            managePrivateChat(parts);
+                            break;
+                    }
+
+                    if (readData[0].Equals('\0'))
+                    {
+                        readData = "Reconnect Again";
+                        msg();
+
+                        this.Invoke((MethodInvoker)delegate // To Write the Received data
+                        {
+                            btnConnect.Enabled = true;
+                        });
+
+                        ctThread.Abort();
+                        clientSocket.Close();
+                        break;
+                    }
+                    chat.Clear();
+                }
+            }
+            catch (Exception e)
+            {
+                ctThread.Abort();
+                clientSocket.Close();
+                btnConnect.Enabled = true;
+                Console.WriteLine(e);
+            }
+
+        }
+
+        private void msg()
+        {
+            if (this.InvokeRequired)
+                this.Invoke(new MethodInvoker(msg));
+            else
+                history.Text = history.Text + Environment.NewLine + " >> " + readData;
+        }
+
+        private void formMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Do you want to exit? ", "Exit", MessageBoxButtons.YesNo);
+            if (dialog == DialogResult.Yes)
+            {
+                try
+                {
+                    ctThread.Abort();
+                    clientSocket.Close();
+                }
+                catch (Exception ee) { }
+
+                Application.ExitThread();
+            }
+            else if (dialog == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+
+        public void managePrivateChat(List<string> parts)
+        {
+
+            this.Invoke((MethodInvoker)delegate // To Write the Received data
+            {
+                if (parts[3].Equals("new"))
+                {
+                    formPrivate privateC = new formPrivate(parts[2], clientSocket, name);
+                    nowChatting.Add(parts[2]);
+                    privateC.Text = "Private Chat with " + parts[2];
+                    privateC.Show();
+                }
+                else
+                {
+                    if (Application.OpenForms["formPrivate"] != null)
+                    {
+                        (Application.OpenForms["formPrivate"] as formPrivate).setHistory(parts[3]);
+                    }
+                }
+
+            });
+
+        }
+
+
+        public byte[] ObjectToByteArray(object _Object)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, _Object);
+                return stream.ToArray();
+            }
+        }
+
+        public Object ByteArrayToObject(byte[] arrBytes)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                var binForm = new BinaryFormatter();
+                memStream.Write(arrBytes, 0, arrBytes.Length);
+                memStream.Seek(0, SeekOrigin.Begin);
+                var obj = binForm.Deserialize(memStream);
+                return obj;
+            }
+        }
+
+        bool SocketConnected(TcpClient s) //check whether client is connected server
+        {
+            bool flag = false;
+            try
+            {
+                bool part1 = s.Client.Poll(10, SelectMode.SelectRead);
+                bool part2 = (s.Available == 0);
+                if (part1 && part2)
+                {
+                    Upddate(true);
+
+                    indicator.BackColor = Color.Red;
+
+                    this.Invoke((MethodInvoker)delegate // cross threads
+                    {
+                        btnConnect.Enabled = true;
+                    });
+                    flag = false;
+                }
+                else
+                {
+                   // lbl.Text = "Server is Connected";
+                    indicator.BackColor = Color.Green;
+                    flag = true;
+
+                }
+            }
+            catch (Exception er)
+            {
+                Console.WriteLine(er);
+            }
+            return flag;
+        }
+
+        private void Upddate(bool v)
+        {
+
+
+
+            Invoke((Action)delegate
+            {
+                lbl.Text = "Server is DisConnected";
+
+                // label1.Visible = textBoxEmployee.Visible = !toggle;
+
+                //  labelIP.Visible = textBoxAddress.Visible = !toggle;
+            }); 
+        }
+
+        private void privateChatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                String clientName = listBox1.GetItemText(listBox1.SelectedItem);
+                chat.Clear();
+                chat.Add("pChat");
+                chat.Add(clientName);
+                chat.Add(name);
+                chat.Add("new");
+
+                byte[] outStream = ObjectToByteArray(chat);
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Flush();
+
+                formPrivate privateChat = new formPrivate(clientName, clientSocket, name);
+                nowChatting.Add(clientName);
+                privateChat.Text = "Private Chat with " + clientName;
+                privateChat.Show();
+                chat.Clear();
+            }
+        }
+
+        private void btnClr_Click(object sender, EventArgs e)
+        {
+            history.Clear();
+        }
+
+        private void history_TextChanged(object sender, EventArgs e)
+        {
+            history.SelectionStart = history.TextLength;
+            history.ScrollToCaret();
+        }
+
+        private void indicator_BackColorChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clientSocket = new TcpClient();
+            
+                clientSocket.Connect("127.0.0.1", 5000);
+                readData = "Connected to Server ";
+
+            //  msg();
+            Updateser(true);
+            serverStream = clientSocket.GetStream();
+
+            byte[] outStream = Encoding.ASCII.GetBytes(name + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+            btnConnect.Enabled = false;
+
+
+            ctThread = new Thread(getMessage);
+            ctThread.Start();
+
+        }
+
+        private void Updateser(bool v)
+        {
+            Invoke((Action)delegate
+            {
+                lbl.Text = "Server is Connected";
+                btnConnect.Enabled = true;
+                btnSend.Enabled = true;
+                btntry.Enabled = false;
+
+
+                // label1.Visible = textBoxEmployee.Visible = !toggle;
+
+                //  labelIP.Visible = textBoxAddress.Visible = !toggle;
+            });
+        }
+
+        private void btnnew_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
